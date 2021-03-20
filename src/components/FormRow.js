d1a7fd7ff7b6307d59e20607a6c49822424e83ef
Vitor Passos <vitorpassos16@gmail.com>
2021-03-20 19:34:38 -0300

    First login with firebase

--- src/components/FormRow.js
+++ src/components/FormRow.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 
 const FormRow = props => {
-    const { children } = props;
+    const { children, first, last } = props;
 
     return (
-        <View style={styles.container}>
+        <View style={[
+            styles.container,
+            first ? styles.first : null,
+            last ? styles.last : null
+        ]}>
             {children}
         </View>
     )
@@ -18,6 +22,12 @@ const styles = StyleSheet.create({
         marginBottom: 5,
         elevation: 1,
         backgroundColor: 'white'
+    },
+    first: {
+        marginTop: 10
+    },
+    last: {
+        marginBottom: 10
     }
 })
 
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,23 +1,94 @@
 import React from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import {
+    View, TextInput, StyleSheet,
+    Button, ActivityIndicator
+} from 'react-native';
 import FormRow from '../components/FormRow';
+import firebase from 'firebase'
 
 export default class LoginPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: '',
+            isLoading: false,
+        }
+    }
+
+    componentDidMount() {
+        const firebaseConfig = {
+            apiKey: "AIzaSyDqs-dnuly5TorI5rprIDh7QZeiFE4YB7Y",
+            authDomain: "series-68b78.firebaseapp.com",
+            projectId: "series-68b78",
+            storageBucket: "series-68b78.appspot.com",
+            messagingSenderId: "762145942008",
+            appId: "1:762145942008:web:ec3eaabb91943b96f88e0d"
+        };
+        // Initialize Firebase
+        if (firebase.apps.length === 0) {
+            firebase.initializeApp(firebaseConfig);
+        }
+    }
+
+    onChangeHandlerInput(key, value) {
+        this.setState({
+            [key]: value
+        });
+    }
+
+    tryLogin() {
+        this.setState({ isLoading: true });
+        const { email, password } = this.state;
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(email, password)
+            .then((user) => {
+                console.log('Autenticado ', user)
+            })
+            .catch((error) => {
+                console.log('Erro Login ', error)
+            }).then(() => {
+                this.setState({ isLoading: false });
+            });
+
+    }
+
+    renderButton() {
+        if (this.state.isLoading) {
+            return <ActivityIndicator size="large" color="#348feb" />;
+        }
+        return (<Button title="Entrar" onPress={() => this.tryLogin()} />);
+    }
+
     render() {
         return (
-            <View>
-                <FormRow>
-                    <TextInput placeholder="Usuário" style={styles.input} />
+            <View style={styles.container}>
+                <FormRow first>
+                    <TextInput placeholder="Email"
+                        style={styles.input}
+                        value={this.state.email}
+                        onChangeText={value => this.onChangeHandlerInput('email', value)} />
                 </FormRow>
-                <FormRow>
-                    <TextInput secureTextEntry={true} placeholder="Senha" style={styles.input} />
+                <FormRow last>
+                    <TextInput secureTextEntry={true}
+                        placeholder="Senha"
+                        style={styles.input}
+                        value={this.state.password}
+                        onChangeText={value => this.onChangeHandlerInput('password', value)} />
                 </FormRow>
+                {this.renderButton()}
             </View>);
     }
 }
 
 const styles = StyleSheet.create({
+    container: {
+        marginLeft: 10,
+        marginRight: 10
+    },
     input: {
-        marginLeft: 12,
+        paddingLeft: 12,
     },
 });
